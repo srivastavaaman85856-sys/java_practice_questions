f20fcf104c18c09b66f81b5f7bba603b81f73cd3
Aman Srivastava <srivastavaaman.85856@gmail.com>
2026-06-12 19:21:28 +0530

    Add files via upload

---
+++ diamond.java
@@ -0,0 +1,32 @@
+class Diamond {
+    public static void main(String[] args){
+        int n=6;
+        for(int i=1;i<n;i++){
+           
+            for(int j=1;j<n-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<i;j++){
+                System.out.print(" *");
+            }
+            for(int j=i-2;j>=1;j--){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+           
+            for(int j=1;j<n-i;j++){
+                System.out.print( "  ");
+            }
+            for(int j=1;j<i;j++){
+                System.out.print(" *");
+            }
+            for(int j=i-2;j>=1;j--){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+    
+}
