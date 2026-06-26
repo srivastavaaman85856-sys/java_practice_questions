7758f130ac5f67bd9eda3d826da2ac20abf293f2
Aman Srivastava <srivastavaaman.85856@gmail.com>
2026-06-26 22:12:47 +0530

    Add files via upload

---
+++ add with function.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void add(int a , int b){
+        System.out.println(a + b);
+    }
+    public static void main(String[] args){
+        add(10 , 20);
+        add(56  ,38);
+    }
+}
